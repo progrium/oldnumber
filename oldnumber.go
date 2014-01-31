bec1da35d343164664e16942a7e03fd38df97747
Jeff Lindsay <progrium@gmail.com>
2014-01-30 20:11:36 -0600

    number correction

--- oldnumber.go
+++ oldnumber.go
@@ -4,19 +4,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
+
+var spokenNumber = "5 1 2, 7 8 5, 1 1 9 4"
+var textNumber = "512 785 1194"
 
 var voiceTwiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-	<Say voice="woman">Hello. Jeff Lindsay no longer receives calls at this number. His new number is 4 0 8, 2 1 8, 0 5 9 6.</Say>
-	<Sms>My new number: 408 218 0596</Sms>
-	<Say voice="woman">If you can receive them, I just texted the number to you. Again, his new number is 4 0 8, 2 1 8, 0 5 9 6. Goodbye.</Say>
+	<Say voice="woman">Hello. Jeff Lindsay no longer receives calls at this number. His new number is, {SPOKEN}.</Say>
+	<Sms>My new number: {TEXT}</Sms>
+	<Say voice="woman">If you can receive them, I just texted the number to your phone. Again, his new number is, {SPOKEN}. Goodbye.</Say>
 </Response>
 `
 
 var smsTwiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-	<Sms>I don't get texts at this number anymore. My new number: 408 218 0596</Sms>
+	<Sms>I don't get texts at this number anymore. My new number: {TEXT}</Sms>
 </Response>
 `
 
@@ -25,6 +29,10 @@ func main() {
 	if port == "" {
 		port = "8888"
 	}
+
+	voiceTwiml = strings.Replace(voiceTwiml, "{SPOKEN}", spokenNumber, -1)
+	voiceTwiml = strings.Replace(voiceTwiml, "{TEXT}", textNumber, -1)
+	smsTwiml = strings.Replace(smsTwiml, "{TEXT}", textNumber, -1)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/xml")
